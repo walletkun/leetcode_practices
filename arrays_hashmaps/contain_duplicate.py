4ef31c981275957513ca9483012f9c827e2f3222
walletkun <feilinpersonal@gmail.com>
2025-01-07 02:12:44 -0500

    -m new study plan, going to revist all data strctures and implement all the necessary algorithms

---
+++ arrays_hashmaps/contain_duplicate.py
@@ -0,0 +1,60 @@
+'''
+Leetcode 217. Contains Duplicate
+Leetcode Link: https://leetcode.com/problems/contains-duplicate/description/
+Difficulty: Easy
+Similar problems: Contains Duplicate II, Contains Duplicate III
+----------------------------
+
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+
+'''
+
+
+def containDuplicate(nums):
+    s = set()
+    for num in nums:
+        if num in s:
+            return True
+        
+        s.add(num)
+
+    return False
+
+
+def main():
+    nums = [1,2,3,1]
+    output = containDuplicate(nums)
+
+    print(output)
+
+if __name__ == '__main__':
+    main()
+        
